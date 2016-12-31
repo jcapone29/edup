2901b2b48b939153362c1123d88540bf9e677c1c
Joe Capone <josephcapone529@gmail.com>
2016-12-31 10:56:33 -0500

    Pushing latest UI upgrades -Muliplatform Navigation

---
+++ Src/PotatoGolf/app/Entities/InstitutionInfo.ts
@@ -0,0 +1,24 @@
+﻿module Potato.Golf {
+
+    export class InstituionInfo {
+        Id: string;
+        UnitId: string;
+        Open_Id_8: string;
+        Open_Id_6: string;
+        Institution_Name: string;
+        City: string;
+        State: string;
+        Zip: string;
+        Accreditor: string;
+        Url: string;
+        Net_Price_Calculator_Url: string;
+        Main_Campus_Flag: string;
+        Branch_Campus_Count: string;
+        Predominant_Degree_Level: string;
+        Highest_Degree_Level: string;
+        Location_Type: string;
+        Latitude: string;
+        Longitude: string;
+        flip = false;
+    }
+}
